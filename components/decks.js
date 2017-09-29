e40b59a16a14cbe0a5a35f73a833c37bba1cc3b3
Thomas Moser <mail@thomasmoser.ch>
2017-09-29 17:29:34 +0200

    initial commit

---
+++ components/decks.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import { Text, View } from 'react-native'
+import styled from 'styled-components/native'
+
+class Decks extends Component {
+  render(){
+    return(
+      <View>
+        <Text>Decks View</Text>
+      </View>
+    )
+  }
+}
+
+export default Decks
