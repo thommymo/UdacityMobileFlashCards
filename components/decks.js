794cb3c445eac9919149ef49c1fce4f5ce6296cd
Thomas Moser <mail@thomasmoser.ch>
2017-10-02 09:27:44 +0200

    feat: Added desks with card numbers on initial screen

--- components/decks.js
+++ components/decks.js
@@ -1,37 +1,50 @@
 import React, {Component} from 'react'
 import { Text, View } from 'react-native'
 import styled from 'styled-components/native'
-import { white, bluelight } from '../utils/colors'
+import { white, bluelight, blue } from '../utils/colors'
 import { connect } from 'react-redux'
 
-const BlueText = styled.Text`
+
+const TitleView = styled.View`
+  min-width: 100%;
+  align-items: center;
+`
+const TitleText = styled.Text`
   color: ${white};
   font-family: "source-sans-pro-light";
   font-size: 40px;
-  flex:1;
+  height: 50px;
 `
-
 const BlueView = styled.View`
   background-color: ${bluelight};
   flex:1;
   align-items: center;
+  justify-content: flex-start;
 `
-
 const DeckView = styled.View`
   flex:1;
+  min-width: 100%;
+  max-height: 120px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${blue};
+  margin-top:1px;
 `
 
 class Decks extends Component {
 
   render(){
+    const { decks } = this.props
     return(
       <BlueView>
-        <BlueText>Decks View</BlueText>
+        <TitleView>
+          <TitleText>Decks View</TitleText>
+        </TitleView>
 
-        {Object.keys(this.props.decks).map((deckname) => (
+        {Object.keys(decks).map((deckname) => (
           <DeckView key={deckname}>
-            <Text>{this.props.decks[deckname].title}</Text>
-            <Text>{this.props.decks[deckname].questions.length}</Text>
+            <Text>{decks[deckname].title}</Text>
+            <Text>{decks[deckname].questions.length} Card(s)</Text>
           </DeckView>
         ))}
 
